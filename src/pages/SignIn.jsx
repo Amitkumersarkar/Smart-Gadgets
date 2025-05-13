0be22416a28775a1f79754ec73b84d89dbc52404
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-14 02:58:38 +0600

    added sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -15,12 +15,12 @@ const SignIn = () => {
         console.log(password);
     }
     return (
-        <div >
-            <h1 className="text-2xl font-bold">User Sign In</h1>
+        <div className="flex flex-col w-75 bg-sky-500 shadow-blue-200 p-10 mt-10 rounded-xl text-center">
+            <h1 className="text-3xl font-bold pb-4">User Sign In</h1>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleEmailChange} type="email" id="email" name="Email" placeholder="Enter Your Email" />
-                <input onChange={handlePasswordChange} type="password" id="password" name="Password" placeholder="Enter Your Password" />
-                <button className="btn btn-ghost bg-amber-600 ml-2">Sign In</button>
+                <input className="pb-2 text-center" onChange={handleEmailChange} type="email" id="email" name="Email" placeholder="Enter Your Email" />
+                <input className="pb-2 text-center" onChange={handlePasswordChange} type="password" id="password" name="Password" placeholder="Enter Your Password" />
+                <button className="btn btn-ghost bg-amber-600 mt-4">Sign In</button>
             </form>
         </div>
     );
