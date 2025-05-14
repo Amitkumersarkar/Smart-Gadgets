064cfeb2a504294f4e8e5c4fe8dc5643bc145b2c
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-15 02:34:07 +0600

    added products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    // load data
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then((res) => res.json())
+            .then((data) => setProducts(data.products));
+    }, [])
     return (
         <div>
-            <h2>product page</h2>
+            <h2>All Products</h2>
+            <section>
+                {/* conditional listing */}
+                {products && products.length > 0 && products.map(product => {
+                    return <article key={product.id}>
+                        <h2>{product.title}</h2>
+                        <p className="p-2">{product.category}</p>
+                        <p>{product.price}</p>
+                        <p>{product.description}</p>
+                    </article>
+                })}
+            </section>
         </div>
     );
 };
