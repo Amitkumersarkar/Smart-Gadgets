66087eae27064a3d113a3ba906c53d3788b841cf
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-15 00:37:34 +0600

    added navigate file

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+
+    const navigate = useNavigate();
+
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
     }
@@ -11,8 +15,14 @@ const SignIn = () => {
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(email);
-        console.log(password);
+        // console.log(email);
+        // console.log(password);
+        if (email === 'amit@gmail.com' && password === '1234') {
+            navigate('/profile');
+        }
+        else {
+            navigate('/');
+        }
     }
     return (
         <div className="flex flex-col w-75 bg-sky-500 shadow-blue-200 p-10 mt-10 rounded-xl text-center">
